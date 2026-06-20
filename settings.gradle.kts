931b932d920202a8f408b5a3cfd53c6ea3a4bd7d
kelven <tan1347@gmail.com>
2026-06-21 02:10:31 +0800

    Optimize compilation speed

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,11 +12,11 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        google()
+        mavenCentral()
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://maven.aliyun.com/repository/public") }
-        google()
-        mavenCentral()
     }
 }
 
